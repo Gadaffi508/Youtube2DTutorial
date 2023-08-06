c9220961a7251c45d9d498d8b8bf732501d65f50
Yusuf <yusuf_zeki1234@hotmail.com>
2023-08-06 13:35:25 +0300

    random list save tyr added

---
+++ Assets/Scripts/Helpv1/CardSystem.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardSystem : MonoBehaviour
+{
+    public List<GameObject> cardsHomeWork = new List<GameObject>();
+    public List<GameObject> cardsGamePlay = new List<GameObject>();
+
+    int random;
+
+    private void Start()
+    {
+        ListActiveFalse(cardsGamePlay);
+        ListActiveFalse(cardsHomeWork);
+
+        cardsGamePlay[GetRandom()].SetActive(true);
+        cardsHomeWork[GetRandom()].SetActive(true);
+
+        random = Random.Range(0, 3);
+
+        Debug.Log(random);
+        Debug.Log(GetRandom());
+
+        if (random == GetRandom())
+        {
+            random = Random.Range(0, cardsGamePlay.Count);
+        }
+
+        cardsGamePlay[random].SetActive(true);
+        cardsHomeWork[random].SetActive(true);
+
+        SaveRandom();
+
+        Debug.Log(random);
+        Debug.Log(GetRandom());
+    }
+
+    public void ListActiveFalse(List<GameObject> cards)
+    {
+        foreach (var card in cards)
+        {
+            card.SetActive(false);
+        }
+    }
+
+    public void SaveRandom()
+    {
+        PlayerPrefs.SetInt("random", random);
+    }
+
+    public int GetRandom()
+    {
+        if (PlayerPrefs.HasKey("random"))
+        {
+            random = PlayerPrefs.GetInt("random");
+            return random;
+        }
+        return 1;
+    }
+}

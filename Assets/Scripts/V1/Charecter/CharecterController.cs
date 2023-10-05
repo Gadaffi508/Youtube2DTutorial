1f7b005bb71a9457e5f06276a46b1c04cbfe0401
Yusuf <yusuf_zeki1234@hotmail.com>
2023-10-05 13:56:55 +0300

    fire ball added

--- Assets/Scripts/V1/Charecter/CharecterController.cs
+++ Assets/Scripts/V1/Charecter/CharecterController.cs
@@ -3,83 +3,55 @@ using UnityEngine;
 
 public class CharecterController : MonoBehaviour
 {
-    [SerializeField] private int speed;
-    [SerializeField] private float JumpForce = 200;
-    [SerializeField] private Transform FirePos;
+    public float runSpeed = 20.0f;
+    public float rotationSpeed = 5.0f; // Dönme hýzý
 
-    LineRenderer lineRenderer;
+    float horizontal;
+    float vertical;
+    float moveLimiter = 0.7f;
+    float targetZ;
+
+    Rigidbody2D body;
     Animator anim;
-    Rigidbody2D rb;
-    float horizontal, Vertical;
-    bool jump;
 
     private void Start()
     {
         anim = GetComponent<Animator>();
-        rb = GetComponent<Rigidbody2D>();
-        lineRenderer = GetComponent<LineRenderer>();
+        body = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
-        Vertical = Input.GetAxis("Vertical");
+        vertical = Input.GetAxis("Vertical");
 
-        anim.SetFloat("speed", Mathf.Abs((horizontal + Vertical)));
+        anim.SetFloat("speed", body.velocity.magnitude);
 
-        if (horizontal > 0) transform.localScale = new Vector3(.7f, .7f, .7f);
-        else if (horizontal < 0) transform.localScale = new Vector3(-.7f, .7f, .7f);
-
-        if (jump && Input.GetKeyDown(KeyCode.Space)) Jump();
-
-        if (Input.GetMouseButtonDown(0)) anim.SetTrigger("Shoot");
+        RotateC();
     }
 
     private void FixedUpdate()
     {
-        transform.Translate(Vector3.right * horizontal * speed * Time.deltaTime);
-    }
-
-    private void Jump()
-    {
-        anim.SetBool("jump", true);
-        jump = false;
-        rb.AddForce(new Vector2(0f, JumpForce));
-    }
-
-    private void OnCollisionEnter2D(Collision2D col)
-    {
-        if (col.gameObject.CompareTag("ground"))
+        if (horizontal != 0 && vertical != 0) // Check for diagonal movement
         {
-            jump = true;
-            anim.SetBool("jump", false);
+            // limit movement speed diagonally, so you move at 70% speed
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
         }
+
+        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
     }
 
-    public void ShootLineRenderer()
+    public void RotateC()
     {
-        RaycastHit2D hitInfo = Physics2D.Raycast(FirePos.position, FirePos.right);
-
-        lineRenderer.enabled = true;
-
-        if (hitInfo)
-        {
-            lineRenderer.SetPosition(0, FirePos.position);
-            lineRenderer.SetPosition(1, hitInfo.point);
-        }
-        else
-        {
-            lineRenderer.SetPosition(0, FirePos.position);
-
-            lineRenderer.SetPosition(1, FirePos.position + FirePos.right * 100);
-        }
-        StartCoroutine(CloseLine());
+        if (vertical > 0 && horizontal != 0) targetZ = 180;
+        if (vertical < 0 && horizontal != 0) targetZ = 0;
+        else if (vertical > 0) targetZ = 180;
+        else if (vertical < 0) targetZ = 0;
+        else if (horizontal > 0) targetZ = 90;
+        else if (horizontal < 0) targetZ = -90;
+        float currentZ = transform.rotation.eulerAngles.z;
+        float newZ = Mathf.LerpAngle(currentZ, targetZ, rotationSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(0, 0, newZ);
     }
-
-    IEnumerator CloseLine()
-    {
-        yield return new WaitForSeconds(.5f);
-        lineRenderer.enabled = false;
-    }
-
 }
